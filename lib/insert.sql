88d674624bfddd9fee071d97a8fae8b4dcd15e7b
github username <github email address>
2017-03-20 16:26:50 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Mr. Chocolate', 2, 'F', 'brown', 'happy', 1),
+('Rowdy', 2, 'F', 'brown', 'happy', 1),
+('Tabitha', 2, 'F', 'brown', 'happy', 0),
+('Sergeant Brown', 2, 'F', 'brown', 'happy', 0),
+('Melissa', 2, 'F', 'brown', 'happy', 1),
+('Grinch', 2, 'F', 'brown', 'happy', 1),
+('Wendy', 2, 'F', 'brown', 'happy', 1),
+(Null, 2, 'F', 'brown', 'happy', 1);
